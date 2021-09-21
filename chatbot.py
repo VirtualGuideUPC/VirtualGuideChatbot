2afd03a89afcd302d68641809d3990a21324ee20
Carhuacusma <dantemoreno2001julio@gmail.com>
2021-09-21 16:32:45 -0500

    Objeto ChatBot
    Se ha adaptado el código a una clase ChatBot, similar al usado en el branch de backend

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ import random
 import numpy as np
 from numpy.lib.function_base import place
 
-from recursos import get_user_location, lemmatizer, filter, make_keywords, fake_query, new_query, query_near, select_names
+from recursos import get_user_location, lemmatizer, filter, make_keywords, new_query, query_near, select_names
 
 from tensorflow.keras.models import load_model
 
@@ -75,18 +75,109 @@ def get_response(intents_list, intents_json):
     # Retornar respuesta de json (String)
     return result
 
+
+class ChatBot:
+    def __init__(self, msg: str, place_context: str):
+        self.message = msg.lower()
+        self.ints = predict_class(self.message)
+        self.place_context = place_context
+        self.responses = []
+        self.intencion = self.ints[0]['intent']
+        self.res = "no fuciona"
+        """
+        ints: lista de objetos (diccionario). 
+        Ejemplo: ints =
+        [{'intents': nombre del tag, 'probabilty': resultado de la neurona},
+        {...}]
+        """
+    
+    def set_message(self, msg):
+        """
+        Analiza el mensaje de entrada hasta extraer una lista de palabras clave.
+        >> msg: Mensaje (string) de entrada
+        Retorna: Lista de keywords
+        """
+        self.message = msg.lower() #...... Mensaje de entrada en minúsculas
+        self.ints = predict_class(self.message) #.. Intención (objeto)
+        self.intencion = self.ints[0]['intent'] #.. Intención (string)
+        aux = filter(self.message, words) # Filtrar las palabras para tener lista tokenizada
+        aux = ' '.join([word.lower() for word in aux])
+        self.responses = []
+        tokens = make_keywords(aux) # Filtro (sintaxis)
+        tokens = [str(token) for token in tokens] # e.g.: ['Museo', 'arte', 'lima']
+        return select_names(keywords=tokens, place_context=self.place_context) # Lista de lugares candidatos
+    def select_candidate(self, place_candidates):
+        """
+        De una lista de lugares 'candidatos', elige o pregunta al usuario
+        """
+        if len(place_candidates) == 1:
+            self.place_context = place_candidates[0]
+        else:
+            print(">> Quiero asegurarme de entenderte bien, ¿a cuál de estos lugares te refieres?")
+            for i in range(len(place_candidates)):
+                print("%s: %s"%(i, place_candidates[i]))
+            message = input("... Por favor, escribe el número: ")
+            self.place_context = place_candidates[int(message)]
+    def create_response(self):
+        """
+        Crea los valores en la lista self.responses
+        (Solamente para las consultas relacionadas a la base de datos)
+        """
+        aux_context = "'%s'"%self.place_context
+        if self.intencion == "consulta_trivia":
+            res = new_query(select_column=['fact'], from_data = "fun_facts", where_pairs=[("touristic_place_id", aux_context)])
+            self.responses = [trivia for trivia in res['fact']].copy()
+        elif self.intencion == "consulta_lugar":
+            res = new_query(select_column=['longitude', 'latitude'], from_data = "touristic_place", where_pairs=[("name", aux_context)])
+            if len(res) > 0:
+                self.responses = ["Las coordenadas de %s son (%s, %s)"%(self.place_context, res.values[0][0], res.values[0][1])].copy()
+        elif self.intencion == "consulta_tiempo":
+            res = new_query(['schedule_info'], "touristic_place", [("name", aux_context)])
+            if len(res) > 0:
+                self.responses = [res.values[0][0]]
+        elif self.intencion == "consulta_precio":
+            res = new_query(['cost_info', 'price'], "touristic_place", [("name", aux_context)])
+            if len(res) > 0:
+                self.responses = ["%s; con precio de %s"%(res.values[0][0], res.values[0][1])]
+        elif self.intencion == "contexto":
+            if self.place_context != " ":
+                self.responses = [self.place_context]
+        elif self.intencion == "consulta_lugares_cerca":
+            res = query_near(get_user_location())
+            self.place_context = res
+            self.responses = ["Encontré: %s"%res]
+    def select_response(self):
+        if len(self.responses) > 0:
+            # Si se hizo una consulta que sí devuelve info:
+            self.res = random.choice(self.responses) # Elegir respuesta al azar
+            return
+        self.res = get_response(self.ints, intents)
+
+    def run(self):
+        """
+        Ejecuta el chatbot en un bucle hasta que sea la despedida
+        """
+        print("Hola! Soy A.V.T... El Asistente Virtual de Turismo, dime, ¿qué puedo hacer por ti?")
+        while True:
+            place_candidates = self.set_message(input("")) # Retorna los posibles lugares del que usuario habla
+            #print("Candidates: ", place_candidates)
+            self.select_candidate(place_candidates)
+            #print("Context after that: ", self.place_context)
+            self.create_response()
+            #print("Responses after that: ", self.responses)
+            self.select_response()
+            print(self.res)
+            if self.intencion == "despedida":
+                break
+        return 0
+
+"""
 print("Hola! Soy A.V.T... El Asistente Virtual de Turismo, dime, ¿qué puedo hacer por ti?")
 place_context = " " # Contexto: (Para continuar consultas sobre un mismo lugar :p)
 while True:
     message = input("")
     message = message.lower()
     ints = predict_class(message)
-    """
-    ints: lista de objetos (diccionario). 
-    Ejemplo: ints =
-    [{'intents': nombre del tag, 'probabilty': resultado de la neurona},
-     {...}]
-    """
     aux = filter(message, words) # Filtrar las palabras (quitar las del json) y tener lista tokenizada
     aux = ' '.join([word.lower() for word in aux]) # Juntar los tokens en un solo string 
     tokens = make_keywords(aux) # Filtrar de nuevo para tener solamente keywords relevantes
@@ -152,3 +243,7 @@ while True:
     # Por defecto (0 respuestas)
     res = get_response(ints, intents)
     print(">>", res)
+"""
+
+AVT = ChatBot(" ", " ")
+AVT.run()
